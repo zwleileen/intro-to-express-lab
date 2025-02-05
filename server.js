62ab97c664dc8ebbb6b45870a818e55ffad371e1
eileen <zwleileen@gmail.com>
2025-02-05 18:00:00 +0800

    finished ex 3

--- server.js
+++ server.js
@@ -18,15 +18,29 @@ app.get("/roll/:number", (req, res) => {
   if (isNaN(number)) {
     return res.send("You must specify a number.");
   }
-  const randomNum = parseInt(number); //converts string input from URL parameter into integer
+  const randomNum = parseInt(number); //converts string input from URL parameter into number
   const result = Math.floor(Math.random() * (randomNum + 1));
   res.send(`You rolled a ${result}`);
 });
 //URL is http://localhost:3000/roll/6
 
-app.get("/", (req, res) => {
-  res.send();
+//Exercise 3. I Want THAT One!
+app.get("/collectibles/:index", (req, res) => {
+  const index = parseInt(req.params.index); //converts URL string parameter into number
+  const collectibles = [
+    { name: "shiny ball", price: 5.95 },
+    { name: "autographed picture of a dog", price: 10 },
+    { name: "vintage 1970s yogurt SOLD AS-IS", price: 0.99 },
+  ];
+
+  if (index < 0 || index >= collectibles.length) {
+    return res.send("This item is not  yet in stock. Check back soon!");
+  }
+  return res.send(
+    `So, you want the ${collectibles[index].name}? For ${collectibles[index].price}, it can be yours!`
+  );
 });
+//URL is http://localhost:3000/collectibles/0
 
 app.get("/about", (req, res) => {
   const name = req.query.name;
